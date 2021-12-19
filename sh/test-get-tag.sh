f746b53c94da948dddd5ae1bc6a32da32b4355d7
蒋金 <ginlink@163.com>
2021-12-19 17:34:10 +0800

    ref: slice num version

--- sh/test-get-tag.sh
+++ sh/test-get-tag.sh
@@ -4,9 +4,9 @@ echo 'starting---------'
 echo $RELEASE_VERSION
 echo '$RELEASE_VERSION'
 
-tag_version=$RELEASE_VERSION
+# tag_version=$RELEASE_VERSION
 
-echo 'slice version'
-echo $tag_version
-echo ${tag_version: 1}
+# echo 'slice version'
+# echo $tag_version
+# echo ${tag_version: 1}
 echo 'ending-----------'

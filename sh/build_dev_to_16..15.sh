faad73366c350b78bead9c6eaea310063f6cf8c7
蒋金 <ginlink@163.com>
2021-12-19 22:12:42 +0800

    fix: 修复docker file

--- sh/build_dev_to_16..15.sh
+++ sh/build_dev_to_16..15.sh
@@ -3,6 +3,10 @@ yarn
 
 yarn build
 
+echo "======================"
+echo $RELEASE_VERSION
+echo "======================"
+
 docker build -t ginlink/test-rollback:$RELEASE_VERSION .
 
 docker login --username $DOCKER_ACCESS_NAME -p $DOCKER_ACCESS_TOKEN

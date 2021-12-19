f71b6fdc289d56034a167d0e322323b522d5cc76
蒋金 <ginlink@163.com>
2021-12-20 06:45:17 +0800

    feat: add node file

--- sh/file.js
+++ sh/file.js
@@ -5,6 +5,16 @@ const path = require('path')
 
 // TODO用流进行操作
 
+function validVersion(version) {
+  const reg = /(\d{1,3}\.){2}\d{1,3}/
+
+  if (!reg.test(version)) {
+    throw new Error(`except version, like 1.0.0, but got ${version}`)
+  }
+
+  return version
+}
+
 function removeLastLineData(filepath) {
   return new Promise((resolve, reject) => {
     let failedResult = {
@@ -16,8 +26,8 @@ function removeLastLineData(filepath) {
 
       const data = fs.readFileSync(filepath, { encoding: 'utf-8' })
 
-      if (!data || data == '\n') {
-        if (data == '\n') {
+      if (!data || data === '\n') {
+        if (data === '\n') {
           fs.writeFileSync(filepath, '')
         }
 
@@ -29,18 +39,50 @@ function removeLastLineData(filepath) {
 
       const arrLines = data.split('\n')
 
-      let resultData = undefined
+      let current = undefined
 
-      while ((resultData = arrLines.pop()) === '') { }
+      // handle last ''
+      while (arrLines[arrLines.length - 1] === '') arrLines.pop()
+
+      let hasValueCurrent = undefined
+      while (true) {
+        current = arrLines.pop()
+
+        // for unique compare
+        if (current) {
+          hasValueCurrent = current
+        }
+
+        // skip ''
+        if (arrLines[arrLines.length - 1] === '') {
+          continue
+        }
+
+        // no left element
+        if (current === undefined) {
+          resolve({
+            success: true,
+            data: undefined
+          })
+          break
+        }
+
+        // unique
+        if (hasValueCurrent !== arrLines[arrLines.length - 1]) {
+          break
+        }
+      }
 
       const newData = arrLines.length ? arrLines.join('\n') : ''
 
       fs.writeFileSync(filepath, newData)
 
+      const lastVersion = arrLines[arrLines.length - 1]
+
       // full success
       resolve({
         success: true,
-        data: resultData,
+        data: lastVersion ? validVersion(lastVersion) : undefined,
       })
     } catch (err) {
       console.log('[](err):', err)
@@ -50,15 +92,14 @@ function removeLastLineData(filepath) {
 
 }
 
-function addLastLine(data, filepath) {
+function addLastLine(version, filepath) {
 
   return new Promise((resolve, reject) => {
-    if (!data.match(/(\d{1,3}\.){2}\d{1,3}/)) {
-      resolve(false)
-    }
+
+    validVersion(version)
 
     try {
-      fs.writeFileSync(filepath, `\n${data}`, {
+      fs.writeFileSync(filepath, `\n${version}`, {
         flag: 'a+'
       })
       resolve(true)
@@ -75,7 +116,7 @@ async function main() {
 
   const filepath = path.join(__dirname, 'version_history.txt')
 
-  // console.log('[]:', await addLastLine('1.0.1', filepath))
+  // console.log('[]:', await addLastLine('1.0.3', filepath))
 
   const result = await removeLastLineData(filepath)
   console.log('[removeLastLineData](result):', result)

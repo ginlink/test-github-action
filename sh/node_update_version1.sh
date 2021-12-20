6e2c90aac9240fbfe6a1c9db126811eec411b352
蒋金 <ginlink@163.com>
2021-12-20 15:58:26 +0800

    u

--- sh/node_update_version1.sh
+++ sh/node_update_version1.sh
@@ -13,20 +13,8 @@ echo "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
 echo "add private key success"
 echo "11111111111111111111111"
 
-# ssh $REMOTE_NAME@$REMOTE_HOST
-
-# mkdir -p ./sh/version
-# localpath=~/sh/version/version_history.txt
-localpath=./version_history.txt
-remotepath=~/sh/version/version_history.txt
-
-# scp $REMOTE_NAME@$REMOTE_HOST:$remotepath ./version_history.txt
 scp $REMOTE_NAME@$REMOTE_HOST:~/sh/version/version_history.txt ./version_history.txt
 
-cat ./version_history.txt
-# sed -i '$a\"$RELEASE_VERSION"' ./sh/version/version_history.txt
+echo '1.0.1' >> ./version_history.txt
 
-# echo $RELEASE_VERSION >> $localpath
-
-# scp $localpath $REMOTE_NAME@$REMOTE_HOST:$remotepath
-
+scp ./version_history.txt $REMOTE_NAME@$REMOTE_HOST:~/sh/version/version_history.txt
--- sh/test.sh
+++ sh/test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-echo $RELEASE_VERSION >> ./sh/version/version_history.txt
+# echo $RELEASE_VERSION >> ./sh/version/version_history.txt
 # sed -i "$a $RELEASE_VERSION" ./sh/version/version_history.txt
+abc=123
+b=0
+echo :$b$abc
